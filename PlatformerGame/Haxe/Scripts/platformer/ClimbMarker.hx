06b4b4cf8f2dc53fd7800e0deabedc24fd1f83da
Cauê Waneck <waneck@gmail.com>
2016-05-28 21:41:05 -0300

    Port ClimbMarker to Haxe
    In this example, you can see how to make templated type calls within Haxe, and how you can use
    more complex uproperty metadata.
    
    First compare the FObjectFinder call with the CreateDefaultSubobject ones. While `FObjectFinder` uses
    type parameter inference, `CreateDefaultSubobject` uses explicit typing by using the dummy `unreal.TypeParam` type.
    These types allow complex expressions to be correctly typed, even though Haxe doesn't allow to explicitly set the
    type parameters that are to be called.
    
    These calls are equivalent, and have the same performance. You may only use the full `unreal.TypeParam` form
    if Haxe fails to correctly inference the resulting type, in which case there will be an error message
    about it.

---
+++ PlatformerGame/Haxe/Scripts/platformer/ClimbMarker.hx
@@ -0,0 +1,28 @@
+package platformer;
+import unreal.*;
+
+@:uclass
+@:uname("APlatformerClimbMarker")
+class ClimbMarker extends AActor {
+  @:uproperty(VisibleDefaultsOnly, BlueprintReadOnly, Category=Mesh, meta=[AllowPrivateAccess="true"])
+  var Mesh:UStaticMeshComponent;
+
+  public function new(wrapped) {
+    super(wrapped);
+
+    var ClimbMarkerOb:FObjectFinder<UStaticMesh> = FObjectFinder.Find("/Game/Environment/meshes/ClimbMarker");
+
+    var init = unreal.FObjectInitializer.Get();
+    var SceneComp = init.CreateDefaultSubobject(new TypeParam<USceneComponent>(), this, "SceneComp", false);
+    RootComponent = SceneComp;
+
+    Mesh = init.CreateDefaultSubobject(new TypeParam<UStaticMeshComponent>(), this, "ClimbMesh", false);
+    Mesh.StaticMesh = ClimbMarkerOb.Object;
+    Mesh.RelativeScale3D = new FVector(0.25, 1.0, 0.25);
+    Mesh.AttachParent = SceneComp;
+  }
+
+  inline public function GetMesh() {
+    return Mesh;
+  }
+}
